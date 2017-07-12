d37617933ae08a25a88a6f7377b352cf1a6f68d2
Aleksey Kholoimov <mykms@ya.ru>
2017-07-12 23:23:40 +0300

    001 Новое. Модель MVP. Начало

---
+++ app/src/main/java/testapp/komcity/ru/myapplicationetalon/PresNews.java
@@ -0,0 +1,15 @@
+package testapp.komcity.ru.myapplicationetalon;
+
+import com.hannesdorfmann.mosby3.mvp.MvpPresenter;
+
+public class PresNews implements MvpPresenter<IPresNews> {
+    @Override
+    public void attachView(IPresNews view) {
+
+    }
+
+    @Override
+    public void detachView(boolean retainInstance) {
+
+    }
+}
