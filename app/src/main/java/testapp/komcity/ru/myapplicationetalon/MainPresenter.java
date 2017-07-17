46154f0b79e07b006f6d39d68517c9cccedb9246
holoimov_a <alex.holoimov@ma.ru>
2017-07-17 18:18:01 +0300

    20170717 Тест Moxy 2

---
+++ app/src/main/java/testapp/komcity/ru/myapplicationetalon/MainPresenter.java
@@ -0,0 +1,12 @@
+package testapp.komcity.ru.myapplicationetalon;
+
+import com.arellomobile.mvp.InjectViewState;
+import com.arellomobile.mvp.MvpPresenter;
+
+@InjectViewState
+public class MainPresenter extends MvpPresenter<MainView> {
+
+    public MainPresenter() {
+        //
+    }
+}
---
+++ app/src/main/java/testapp/komcity/ru/myapplicationetalon/MainView.java
@@ -0,0 +1,7 @@
+package testapp.komcity.ru.myapplicationetalon;
+
+import com.arellomobile.mvp.MvpView;
+
+public interface MainView extends MvpView {
+    void isProgressing(boolean isProgress);
+}
