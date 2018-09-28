e6fdb91e3406b38be0f591a618644ae880abce13
holoimov <Hol!3291>
2018-09-28 12:17:37 +0300

    20180928 Доработка. Переименовали пакет 2

--- app/src/androidTest/java/ru/komcity/mobile/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/ru/komcity/mobile/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("ru.komcity.android", appContext.getPackageName());
+        assertEquals("ru.komcity.mobile", appContext.getPackageName());
     }
 }
