3ac1371b20e11425f9d0a3e7d258cd2bf99b1b05
Aleksey <developkms@ya.ru>
2019-03-13 22:23:23 +0300

    20190313 Project java to kotlin with AndroidX

--- app/src/main/java/ru/komcity/mobile/MainActivity.kt
+++ app/src/main/java/ru/komcity/mobile/MainActivity.kt
@@ -1,2 +1,11 @@
 package ru.komcity.mobile
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
